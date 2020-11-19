3186faad7cffd459f40ff5974b65bcfdb8bd76be
ebina4yaka <4yaka3bina@gmail.com>
2020-11-20 08:42:10 +0900

    add test

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 generate:
 	go run github.com/99designs/gqlgen && go run ./model_tags.go
+test:
+	go test ./tests
--- model_tags.go
+++ model_tags.go
@@ -3,27 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strings"
 
 	"github.com/99designs/gqlgen/api"
 	"github.com/99designs/gqlgen/codegen/config"
 	"github.com/99designs/gqlgen/plugin/modelgen"
+	"github.com/ebina4yaka/gqlgen-api-example/lib"
 )
-
-var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-
-func ToSnakeCase(str string) string {
-	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-	return strings.ToLower(snake)
-}
 
 func addBsonTags(b *modelgen.ModelBuild) *modelgen.ModelBuild {
 	for _, model := range b.Models {
 		for _, field := range model.Fields {
-			field.Tag += ` bson:"` + ToSnakeCase(field.Name) + `"`
+			field.Tag += ` bson:"` + lib.ToSnakeCase(field.Name) + `"`
 		}
 	}
 
